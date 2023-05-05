79c59f32fa87f0d876fca0af6643e3d485d6f3db
grannyprogramming <amcg809@gmail.com>
2023-05-05 23:07:00 +0100

    changing create env vars to use subprocess

--- .github/utils/setEnvVariables.py
+++ .github/utils/setEnvVariables.py
@@ -1,19 +1,20 @@
 import argparse
 import json
-import subprocess
+import os
+from pathlib import Path
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file_path", help="path to JSON file containing environment variables")
     args = parser.parse_args()
 
-    env_file = args.file_path
-    with open(env_file) as f:
+    env_file = Path(args.file_path)
+    with env_file.open() as f:
         env_vars = json.load(f)
-    
     for key, value in env_vars.items():
-        print(f"Setting environment variable {key.upper()}")
-        subprocess.run(['export', f"{key.upper()}='{value}'"], shell=True)
+        env_var = f"{key.upper()}={value}"
+        print(f"Setting environment variable {env_var}")
+        os.system(f"echo {env_var} >> $GITHUB_ENV")
 
 if __name__ == "__main__":
     main()

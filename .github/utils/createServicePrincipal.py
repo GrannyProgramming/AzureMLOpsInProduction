6d631b2c261379f5bfbc499bfc0a025a3450b80a
grannyprogramming <amcg809@gmail.com>
2023-05-05 22:24:05 +0100

    creating sp

--- .github/utils/createServicePrincipal.py
+++ .github/utils/createServicePrincipal.py
@@ -3,19 +3,12 @@ import os
 import json
 
 # Replace these values with your own
-subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
-sp_name = os.environ["AZURE_SP_NAME"]
+subscription_id = os.environ["SUBSCRIPTION_ID"]
+sp_name = os.environ["SP_NAME"]
 
 # Create a new GitHub App
 app_create_command = f"gh auth login"
 subprocess.run(app_create_command, capture_output=True, shell=True)
-
-app_create_command = f"gh api /authorizations -X POST -H 'Accept: application/vnd.github.v3+json' -d '{{\"scopes\": [\"admin:org\", \"repo\"], \"note\": \"My authorization\"}}'"
-app_response = subprocess.run(app_create_command, capture_output=True, shell=True)
-
-# Get the GitHub token
-gh_token = app_response.stdout.decode("utf-8")
-gh_token = gh_token.split(": ")[1].strip()
 
 # Create a new Azure Service Principal
 az_create_command = f"az ad sp create-for-rbac --name {sp_name} --role Contributor --scopes /subscriptions/{subscription_id} --sdk-auth"
@@ -31,11 +24,3 @@ subprocess.run(az_login_command, shell=True)
 # Set the Azure subscription context
 az_set_subscription_command = f"az account set --subscription {subscription_id}"
 subprocess.run(az_set_subscription_command, shell=True)
-
-# Set the GitHub App token as an environment variable
-set_gh_token_command = f"export GH_TOKEN={gh_token}"
-subprocess.run(set_gh_token_command, shell=True)
-
-# Authenticate with the GitHub App
-gh_auth_command = f"gh auth login --with-token <<< '${{ GH_TOKEN }}'"
-subprocess.run(gh_auth_command, shell=True)

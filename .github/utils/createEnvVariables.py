b8d03c6e1ca585a4814c2e392968b04036a587cb
grannyprogramming <amcg809@gmail.com>
2023-05-05 21:22:56 +0100

    correcting dependencies

--- .github/utils/createEnvVariables.py
+++ .github/utils/createEnvVariables.py
@@ -1,28 +1,20 @@
-import os
+import argparse
 import json
+import os
+from pathlib import Path
 
-def set_environment_variables(file_path):
-    """
-    Set environment variables from a JSON file.
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_path", help="path to JSON file containing environment variables")
+    args = parser.parse_args()
 
-    Args:
-        file_path (str): The path to the JSON file containing the variables.
+    env_file = Path(args.file_path)
+    with env_file.open() as f:
+        env_vars = json.load(f)
+    for key, value in env_vars.items():
+        print(f"Setting environment variable {key}")
+        print(f"Value: {value}")
+        os.environ[key] = value
 
-    Raises:
-        FileNotFoundError: If the specified file path does not exist.
-        KeyError: If the specified JSON file does not contain all required variables.
-
-    """
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"File not found at {file_path}")
-
-    # Read JSON file
-    with open(file_path, 'r') as f:
-        variables = json.load(f)
-
-    # Set environment variables
-    required_vars = ['subscription_id', 'resource_group', 'workspace']
-    for var in required_vars:
-        if var not in variables:
-            raise KeyError(f"Missing required variable '{var}' in file '{file_path}'")
-        os.environ[var.upper()] = variables[var]
+if __name__ == "__main__":
+    main()

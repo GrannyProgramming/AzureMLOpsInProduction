c98fc4518b71f876cadec519267febecd3157ccc
GrannyProgramming <amcg809@gmail.com>
2023-05-11 08:11:38 +0100

    SP_CREATION

--- .github/utils/createServicePrincipal.py
+++ .github/utils/createServicePrincipal.py
@@ -1,47 +1,64 @@
+import os
 import subprocess
 import json
-import requests
-import os
 
-# Define the required GitHub API parameters
-github_url = os.environ["GH_ADDRESS"]
+# Ensure Azure CLI is installed
+assert subprocess.run(["az", "--version"], capture_output=True).returncode == 0, "Azure CLI not installed"
 
-# Define the required GitHub CLI command to create a token
-gh_cli_command = 'gh auth login --with-token -s repo'
+# Ensure environment variables are set
+assert "SUBSCRIPTION_ID" in os.environ, "SUBSCRIPTION_ID environment variable not set"
+assert "SP_NAME" in os.environ, "SP_NAME environment variable not set"
+assert "GH_TOKEN" in os.environ, "GH_TOKEN environment variable not set"
+assert "GH_OWNER" in os.environ, "GH_OWNER environment variable not set"
+assert "GH_REPO" in os.environ, "GH_REPO environment variable not set"
 
-# Execute the GitHub CLI command and capture the output
-output1 = subprocess.check_output(gh_cli_command.split()).decode('utf-8')
+subscription_id = os.environ["SUBSCRIPTION_ID"]
+sp_name = os.environ["SP_NAME"]
+gh_token = os.environ["GH_TOKEN"]
+gh_owner = os.environ["GH_OWNER"]
+gh_repo = os.environ["GH_REPO"]
 
-# Parse the output to retrieve the token
-token = output1.split('\n')[0].strip()
+# Set the subscription id
+subprocess.run(["az", "account", "set", "--subscription", subscription_id], check=True)
 
-az_cli_command = f'az ad sp create-for-rbac -n {os.environ["SP_NAME"]} --sdk-auth'
+# Create the service principal
+sp_create_output = subprocess.run(
+    ["az", "ad", "sp", "create-for-rbac", "-n", sp_name, "--skip-assignment"],
+    capture_output=True,
+    check=True,
+    text=True,
+)
 
-# Execute the Azure CLI command and capture the output
-output = subprocess.check_output(az_cli_command.split()).decode('utf-8')
+# Parse the json output
+sp_create_output_json = json.loads(sp_create_output.stdout)
 
-# Parse the output to retrieve the necessary information
-credentials = json.loads(output)
-client_id = credentials['clientId']
-client_secret = credentials['clientSecret']
-tenant_id = credentials['tenantId']
-subscription_id = credentials['subscriptionId']
+# Get the appId, password, and tenant
+app_id = sp_create_output_json["appId"]
+password = sp_create_output_json["password"]
+tenant_id = sp_create_output_json["tenant"]
 
-secrets = {
-    'AZURE_CLIENT_ID': client_id,
-    'AZURE_CLIENT_SECRET': client_secret,
-    'AZURE_TENANT_ID': tenant_id,
-    'AZURE_SUBSCRIPTION_ID': subscription_id
-}
+print(f"Created service principal with appId: {app_id}")
 
-# Loop through the secrets and create/update them on GitHub
-for name, value in secrets.items():
-    secret_url = github_url + '/' + name
-    data = {'encrypted_value': value.encode('utf-8').hex()}
-    headers = {'Authorization': f'token {token}'}
-    response = requests.put(secret_url, headers=headers, json=data)
+# Log in to Azure with the service principal
+subprocess.run(
+    ["az", "login", "--service-principal", "-u", app_id, "-p", password, "--tenant", tenant_id],
+    check=True,
+)
 
-    if response.status_code == 204:
-        print(f'Successfully created/updated secret: {name}')
-    else:
-        print(f'Failed to create/update secret: {name}')
+# Create GitHub secrets for appId, password, tenant id, subscription id, and the full output
+for secret_name, secret_value in {"AZURE_CLIENT_ID": app_id, "AZURE_TENANT_ID": tenant_id, "AZURE_SUBSCRIPTION_ID": subscription_id, "AZURE_SECRET": password, "AZURE_CREDENTIALS": json.dumps(sp_create_output_json)}.items():
+    subprocess.run(
+        [
+            "curl",
+            "-X",
+            "PUT",
+            f"https://api.github.com/repos/{gh_owner}/{gh_repo}/actions/secrets/{secret_name}",
+            "-H",
+            "Accept: application/vnd.github.v3+json",
+            "-H",
+            f"Authorization: token {gh_token}",
+            "-d",
+            json.dumps({"encrypted_value": secret_value, "key_id": gh_token}),
+        ],
+        check=True,
+    )

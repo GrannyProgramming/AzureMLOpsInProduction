73ef3048a82dccbeec1ec022063283f9dfa959d5
GrannyProgramming <amcg809@gmail.com>
2023-05-24 12:03:10 +0100

    optimised script, removed jq and used ps1 functionality

--- quickstart.ps1
+++ quickstart.ps1
@@ -3,10 +3,9 @@ Azure CLI (az): The Azure CLI is a command-line tool that you can use to manage 
 
 GitHub CLI (gh): GitHub CLI brings GitHub to your terminal. It reduces context switching, helps you focus, and enables you to more easily script and create automation. It is used in this script to set GitHub secrets. You can install it by following the instructions on this page: https://cli.github.com/manual/installation 
 
-jq: jq is a lightweight and flexible command-line JSON processor. It is like sed for JSON data. It is used in this script to parse the JSON output of the Azure CLI command. You can install it by following the instructions on this page: https://stedolan.github.io/jq/download/
-
 Restart terminal after installing all the above
 '''
+
 # Define the variables
 $PARAMS_FILE = $args[0]  # Accept the JSON file as the first command-line argument
 
@@ -39,19 +38,19 @@ az login
 az account set --subscription $SUBSCRIPTION_ID
 
 # Create the service principal
-$sp = az ad sp create-for-rbac --name $SP_NAME --sdk-auth
+$sp = az ad sp create-for-rbac --name $SP_NAME --sdk-auth | ConvertFrom-Json
 
 # Save the service principal credentials to GitHub Secrets
-# NOTE: You need to have 'jq' utility installed in your system or use an equivalent in PowerShell to parse the JSON output
-$clientId = $(echo $sp | jq -r '.clientId')
-$clientSecret = $(echo $sp | jq -r '.clientSecret')
-$tenantId = $(echo $sp | jq -r '.tenantId')
+$clientId = $sp.clientId
+$clientSecret = $sp.clientSecret
+$tenantId = $sp.tenantId
 
 gh auth login 
+
 # Set the GitHub secrets using GitHub CLI
-echo $clientId | gh secret set AZURE_CLIENT_ID -r "$GH_OWNER/$GH_REPO" --body "$clientId"
-echo $clientSecret | gh secret set AZURE_CLIENT_SECRET -r "$GH_OWNER/$GH_REPO" --body "$clientSecret"
-echo $tenantId | gh secret set AZURE_TENANT_ID -r "$GH_OWNER/$GH_REPO" --body "$tenantId"
-echo $sp | gh secret set AZURE_CREDENTIALS -r "$GH_OWNER/$GH_REPO" --body "$sp"
+gh secret set AZURE_CLIENT_ID -r "$GH_OWNER/$GH_REPO" --body "$clientId"
+gh secret set AZURE_CLIENT_SECRET -r "$GH_OWNER/$GH_REPO" --body "$clientSecret"
+gh secret set AZURE_TENANT_ID -r "$GH_OWNER/$GH_REPO" --body "$tenantId"
+gh secret set AZURE_CREDENTIALS -r "$GH_OWNER/$GH_REPO" --body ($sp | ConvertTo-Json)
 
 echo "Service principal created and saved in GitHub Secrets"

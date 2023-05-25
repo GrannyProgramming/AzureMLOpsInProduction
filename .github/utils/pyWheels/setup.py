c2c20005376dc4955c77a93a9b2d6318e56aee16
GrannyProgramming <amcg809@gmail.com>
2023-05-25 12:51:49 +0100

    Moving wheel file info into seperate folder for better organization

---
+++ .github/utils/pyWheels/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='loggingConfig',
+    version='1.0.1',
+    author='amcg809',
+    author_email='amcg809@gmail.com',
+    description='A logging utility package',
+    py_modules=['loggingConfig'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

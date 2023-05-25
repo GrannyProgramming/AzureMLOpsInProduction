c282c1b5457940a33de54a0e992960c05e421383
GrannyProgramming <amcg809@gmail.com>
2023-05-25 09:47:08 +0100

    removed installation of gh cli and changed filepath

--- .github/utils/installDependencies.py
+++ .github/utils/installDependencies.py
@@ -1,4 +1,7 @@
-'''This code installs various dependencies and the Github CLI'''
+'''
+This code installs various dependencies and the Github CLI
+
+'''
 
 import subprocess
 
@@ -14,17 +17,17 @@ def install_dependencies():
     if subprocess.run(['which', 'bicep']).returncode != 0:
         subprocess.run(['curl', '-Lo', '/usr/local/bin/bicep', 'https://github.com/Azure/bicep/releases/latest/download/bicep-linux-x64'])
         subprocess.run(['chmod', '+x', '/usr/local/bin/bicep'])
-    if subprocess.run(['which', 'curl']).returncode != 0:
-        subprocess.run(['sudo', 'apt', 'update'])
-        subprocess.run(['sudo', 'apt', 'install', 'curl', '-y'])
-    subprocess.run(['curl', '-fsSL', 'https://cli.github.com/packages/githubcli-archive-keyring.gpg'], stdout=subprocess.PIPE)
-    curl_process = subprocess.run(['curl', '-fsSL', 'https://cli.github.com/packages/githubcli-archive-keyring.gpg'], stdout=subprocess.PIPE)
-    subprocess.run(['sudo', 'dd', 'of=/usr/share/keyrings/githubcli-archive-keyring.gpg'], input=curl_process.stdout)
-    subprocess.run(['sudo', 'chmod', 'go+r', '/usr/share/keyrings/githubcli-archive-keyring.gpg'])
-    subprocess.run(['echo', f"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main"], stdout=subprocess.PIPE, shell=True)
-    subprocess.run(['sudo', 'tee', '/etc/apt/sources.list.d/github-cli.list'], stdin=subprocess.PIPE)
-    subprocess.run(['sudo', 'apt', 'update'])
-    subprocess.run(['sudo', 'apt', 'install', 'gh', '-y'])
+    # if subprocess.run(['which', 'curl']).returncode != 0:
+    #     subprocess.run(['sudo', 'apt', 'update'])
+    #     subprocess.run(['sudo', 'apt', 'install', 'curl', '-y'])
+    # subprocess.run(['curl', '-fsSL', 'https://cli.github.com/packages/githubcli-archive-keyring.gpg'], stdout=subprocess.PIPE)
+    # curl_process = subprocess.run(['curl', '-fsSL', 'https://cli.github.com/packages/githubcli-archive-keyring.gpg'], stdout=subprocess.PIPE)
+    # subprocess.run(['sudo', 'dd', 'of=/usr/share/keyrings/githubcli-archive-keyring.gpg'], input=curl_process.stdout)
+    # subprocess.run(['sudo', 'chmod', 'go+r', '/usr/share/keyrings/githubcli-archive-keyring.gpg'])
+    # subprocess.run(['echo', f"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main"], stdout=subprocess.PIPE, shell=True)
+    # subprocess.run(['sudo', 'tee', '/etc/apt/sources.list.d/github-cli.list'], stdin=subprocess.PIPE)
+    # subprocess.run(['sudo', 'apt', 'update'])
+    # subprocess.run(['sudo', 'apt', 'install', 'gh', '-y'])
 
 
 install_dependencies()

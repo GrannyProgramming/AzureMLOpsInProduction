85d43418337204e1e51a3d62d996f06ec51b3327
GrannyProgramming <amcg809@gmail.com>
2023-05-26 11:36:20 +0100

    ds

--- .github/utils/installDependencies.py
+++ .github/utils/installDependencies.py
@@ -15,7 +15,7 @@ def install_dependencies():
     subprocess.run(['az', 'extension', 'add', '-n', 'azure-cli-ml'])
     subprocess.run(['pip3', 'install', 'azure-ai-ml'])
     subprocess.run(['pip3', 'install', 'mltable'])
-    subprocess.run(['pip3', 'install', 'requests'])
+    subprocess.run(['pip3', 'install', 'requests'])  
     # if subprocess.run(['which', 'bicep']).returncode != 0:
     #     subprocess.run(['curl', '-Lo', '/usr/local/bin/bicep', 'https://github.com/Azure/bicep/releases/latest/download/bicep-linux-x64'])
     #     subprocess.run(['chmod', '+x', '/usr/local/bin/bicep'])

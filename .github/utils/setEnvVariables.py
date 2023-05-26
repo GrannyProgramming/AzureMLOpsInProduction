ebca3b4d0f211db800c346845d90d85171e494a1
GrannyProgramming <amcg809@gmail.com>
2023-05-26 17:12:02 +0100

    Updating with a python script to create envs using new .whl

--- .github/utils/setEnvVariables.py
+++ .github/utils/setEnvVariables.py
@@ -1,22 +1,19 @@
-# Potentially make this a helper function in the future
-
-import argparse
-import json
-import os
+import sys
+from workflowHelperFunc.your_module import load_and_set_env_vars
 from pathlib import Path
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("file_path", help="path to JSON file containing environment variables")
-    args = parser.parse_args()
+    # The first argument (sys.argv[1]) is the command line argument
+    vars_or_path = sys.argv[1]
 
-    env_file = Path(args.file_path)
-    with env_file.open() as f:
-        env_vars = json.load(f)
-    for key, value in env_vars.items():
-        env_var = f"{key.upper()}={value}"
-        print(f"Setting environment variable {env_var}")
-        os.system(f"echo {env_var} >> $GITHUB_ENV")
-        
+    # Check if the argument is a file path or a list of variables
+    if Path(vars_or_path).exists():
+        # If it's a file path, pass it as the 'file_path' parameter
+        load_and_set_env_vars(file_path=vars_or_path)
+    else:
+        # If it's not a file path, split the string into a list of variables
+        var_list = vars_or_path.split()
+        load_and_set_env_vars(var_list=var_list)
+
 if __name__ == "__main__":
     main()

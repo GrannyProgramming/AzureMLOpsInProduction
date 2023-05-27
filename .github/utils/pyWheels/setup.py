097dea9731fd3634f426aa681456350b4c876424
GrannyProgramming <amcg809@gmail.com>
2023-05-27 09:08:26 +0100

    ds

---
+++ .github/utils/pyWheels/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='workflowhelperfunc',
+    version='1.0.1',
+    author='amcg809',
+    author_email='amcg809@gmail.com',
+    description='A helper function for the workflow (Logging, setEnvVariables, etc.))',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
+ 

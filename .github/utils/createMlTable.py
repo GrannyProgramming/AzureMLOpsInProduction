f73c900e0413e1ad5389a9421bbe111612bf6116
GrannyProgramming <amcg809@gmail.com>
2023-06-02 13:20:00 +0100

    removing uri stuff as to reduce comlexity

--- .github/utils/createMlTable.py
+++ .github/utils/createMlTable.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 def create_directories(save_dir):
     """
-    Create necessary directories based on the type of the provided file path.
+    Create necessary directories.
 
     Args:
         save_dir (str): The directory where the processed table will be saved.
@@ -13,24 +13,13 @@ def create_directories(save_dir):
     save_dir = Path(save_dir)
     save_dir.mkdir(parents=True, exist_ok=True)
 
-    # Define subdirectories for MLTable and URI
+    # Define subdirectories for MLTable 
     mltable_dir = save_dir / "MLTable"
-    uri_dir = save_dir / "URI"
-
-    # Check the type of the provided file path
-    if mltable_dir.is_file():
-        # Provided path is an MLTable file
-        mltable_dir = mltable_dir.parent
-    elif uri_dir.is_file():
-        # Provided path is a URI file
-        uri_dir = uri_dir.parent
-
-    # Create the necessary directories
+    
+    # Create the subdirectories if they do not exist
     mltable_dir.mkdir(parents=True, exist_ok=True)
-    uri_dir.mkdir(parents=True, exist_ok=True)
-
-    return mltable_dir, uri_dir
-
+    
+    return mltable_dir
 
 def load_data():
     """
@@ -66,21 +55,19 @@ def preprocess_data(tbl):
 
     return tbl
 
-def save_data(tbl, mltable_dir, uri_dir):
+def save_data(tbl, mltable_dir):
     """
     Save the processed data.
 
     Args:
         tbl (mltable): The preprocessed mltable.
         mltable_dir (Path): Directory to save the mltable.
-        uri_dir (Path): Directory to save the parquet file.
     """
     # Save the ML table
     tbl.save(str(mltable_dir))
 
     # Convert the ML table to a pandas dataframe and save as Parquet
-    df = tbl.to_pandas_dataframe()
-    df.to_parquet(str(uri_dir / "NYC_taxi.parquet"))
+    df = tbl.to_pandas()
 
 def main():
     """
@@ -90,10 +77,10 @@ def main():
     parser.add_argument('save_directory', type=str, help='Directory to save data')
     args = parser.parse_args()
 
-    mltable_dir, uri_dir = create_directories(args.save_directory)
+    mltable_dir = create_directories(args.save_directory)
     tbl = load_data()
     tbl = preprocess_data(tbl)
-    save_data(tbl, mltable_dir, uri_dir)
+    save_data(tbl, mltable_dir)
 
 if __name__ == "__main__":
     main()

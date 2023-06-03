51dc64862a93c44769b9d37111407d5eea258e13
GrannyProgramming <amcg809@gmail.com>
2023-06-03 13:19:13 +0100

    adding try except caluse

--- .github/utils/createEnvironments.py
+++ .github/utils/createEnvironments.py
@@ -14,48 +14,45 @@ logging.basicConfig(level=logging.INFO)
 
 # 3. Define the function that creates environments according to their types specified in the JSON configuration
 def create_environment_from_json(env_config):
-    # Check if environment already exists with the same version
-    env_list = ml_client.environments.list(name=env_config['name'])
-    env_versions = [env.version for env in env_list]
-
-    if env_config.get('version') in env_versions:
+    try:
+        # Try to get the specific environment version
+        env = ml_client.environments.get(name=env_config['name'], version=env_config.get('version'))
         logging.info(f"Environment {env_config['name']} with version {env_config.get('version')} already exists. Skipping...")
-        return
+    except azure.core.exceptions.ResourceNotFoundError:
+        # If the environment doesn't exist, create it
+        if 'image' in env_config:
+            env = Environment(
+                name=env_config['name'],
+                version=env_config.get('version'),
+                description=env_config.get('description'),
+                image=env_config['image'],
+            )
+            ml_client.environments.create_or_update(env)
 
-    if 'image' in env_config:
-        env = Environment(
-            name=env_config['name'],
-            version=env_config.get('version'),
-            description=env_config.get('description'),
-            image=env_config['image'],
-        )
-        ml_client.environments.create_or_update(env)
+        elif 'path' in env_config:
+            env = Environment(
+                name=env_config['name'],
+                version=env_config['version'],
+                build=BuildContext(path=env_config['path']),
+            )
+            ml_client.environments.create_or_update(env)
 
-    elif 'path' in env_config:
-        env = Environment(
-            name=env_config['name'],
-            version=env_config['version'],
-            build=BuildContext(path=env_config['path']),
-        )
-        ml_client.environments.create_or_update(env)
+        elif 'channels' in env_config and 'dependencies' in env_config:
+            conda_dependencies = {
+                'channels': env_config['channels'],
+                'dependencies': env_config['dependencies']
+            }
 
-    elif 'channels' in env_config and 'dependencies' in env_config:
-        conda_dependencies = {
-            'channels': env_config['channels'],
-            'dependencies': env_config['dependencies']
-        }
-
-        conda_file = env_config['name'] + '.yml'
-        # with open(conda_file, 'w') as file:
-        #     documents = yaml.dump(conda_dependencies, file)
-        
-        env = Environment(
-            name=env_config['name'],
-            version=env_config['version'],
-            conda_file=conda_file,
-        )
-        ml_client.environments.create_or_update(env)
-
+            conda_file = env_config['name'] + '.yml'
+            with open(conda_file, 'w') as file:
+                documents = yaml.dump(conda_dependencies, file)
+            
+            env = Environment(
+                name=env_config['name'],
+                version=env_config['version'],
+                conda_file=documents,
+            )
+            ml_client.environments.create_or_update(env)
 
 # Check if the script is invoked with necessary arguments
 if len(sys.argv) < 2:

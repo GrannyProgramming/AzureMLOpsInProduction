8bc17d0345900eae875bf331effa6cbecbc26931
GrannyProgramming <amcg809@gmail.com>
2023-06-07 10:23:43 +0100

    trying .list

--- .github/utils/createEnvironments.py
+++ .github/utils/createEnvironments.py
@@ -9,32 +9,48 @@ from workflowhelperfunc.workflowhelper import initialize_mlclient
 ml_client = initialize_mlclient()
 
 # 3. Define the function that creates environments according to their types specified in the JSON configuration
+import json
+import yaml
+import sys
+from azureml.core import Workspace, Environment
+from azureml.exceptions import WebserviceException
+
+# 2. Configure workspace details and get a handle to the workspace
+ws = Workspace.from_config()
+
+# 3. Define the function that creates environments according to their types specified in the JSON configuration
 def create_environment_from_json(env_config):
     # Initialize version
     new_version = '1'
 
     # Check if environment already exists
     try:
-        versions = ml_client.environments.list_versions(env_config['name'])
-        existing_versions = sorted([version.version for version in versions])
+        # Fetch all versions of the environment
+        env_versions = Environment.list(ws, env_config['name'])
+        existing_versions = sorted([int(version.split('_')[-1]) for version in env_versions.keys()])
         
         if existing_versions:
             # If existing versions are found, set the new version to be the incremented last version
             new_version = str(int(existing_versions[-1]) + 1)
-            existing_env = ml_client.environments.get(name=env_config['name'], version=existing_versions[-1])
+
+        # If version is not auto, set new version as given version
+        if env_config['version'].lower() != 'auto':
+            new_version = env_config['version']
+
+        # Get the latest environment version
+        existing_env = None
+        if existing_versions:
+            existing_env = Environment.get(ws, name=env_config['name'], version=str(existing_versions[-1]))
             
-            if existing_env and existing_env.conda_file == env_config.get('conda_file', '') and env_config['version'].lower() == 'auto':
-                print(f"Environment with name {env_config['name']} and version {env_config['version']} already exists and dependencies are the same.")
+            # Check if the conda dependencies are the same and version is set to 'auto'
+            if existing_env and existing_env.python.conda_dependencies.serialize_to_string() == env_config.get('conda_file', '') and env_config['version'].lower() == 'auto':
+                print(f"Environment with name {env_config['name']} and version {existing_env.version} already exists and dependencies are the same.")
                 return
             elif env_config['version'].lower() != 'auto':
                 print(f"Warning: SKIPPING ENVIRONMENT CREATION - Environment {env_config['name']} version and name are same but dependencies have changed. Version is not set to 'auto', so version will not be incremented.")
                 return
             else:
                 print("Dependencies have changed or version is set to 'auto', incrementing version and updating environment.")
-        elif env_config['version'].lower() != 'auto':
-            # If no existing versions and version is not auto, set new version as given version
-            new_version = env_config['version']
-
     except Exception as e:
         print(f"An error occurred: {str(e)}. Proceeding to creation/update...") 
         

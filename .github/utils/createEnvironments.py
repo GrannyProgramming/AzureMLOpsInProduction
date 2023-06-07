b89faef0d0eafb73f3fae2d818cf114075ac9ea9
GrannyProgramming <amcg809@gmail.com>
2023-06-07 13:23:15 +0100

    overhaul of attempt

--- .github/utils/createEnvironments.py
+++ .github/utils/createEnvironments.py
@@ -20,19 +20,71 @@ def create_environment_from_json(env_config):
         existing_envs_sorted = sorted(existing_envs, key=lambda e: int(e.version), reverse=True)
         existing_env = existing_envs_sorted[0]  # The latest version
         print(f"DEBUG: Existing environment: {existing_env}")
-    
-    # elif existing_env.conda_file == env_config.get('conda_file', '') and existing_env.version == env_config['version'] and env_config['version'].lower() != 'auto':
-    #     print(f"Warning: SKIPPING ENVIRONMENT CREATION - Environment {env_config['name']} version and name are same but dependencies have changed. Version is not set to 'auto', so version will not be incremented.")
-    #     return
-    # elif existing_env and not existing_env.conda_file:
-    #     print("Warning: The environment exists but there are no dependencies specified.")
-    #     return
-    # elif existing_env:
-    #     print("Dependencies have changed or version is set to 'auto', incrementing version and updating environment.")
-    #     new_version = str(int(existing_env.version) + 1)  # Increment the version number
-    # elif env_config['version'].lower() != 'auto':
-    #     # If environment doesn't exist and version is not auto, set new version as given version
-    #     new_version = env_config['version']
+    else:  # No existing environment, create new one
+        env_config['version'] = new_version if env_config['version'] == 'auto' else env_config['version']
+
+    # try:
+    # selcected scenarios go here
+
+    # except Exception as e:
+
+    env = None
+    if 'build' in env_config:
+        # Build Context case
+        # Create build context
+        build_context = BuildContext(path=env_config['build'])
+
+        # Compare existing AML build context with new build context
+        if existing_env and existing_env.build == build_context:
+            print(f"The build context for {env_config['name']} matches the existing one.")
+        else:
+            env = Environment(
+                name=env_config['name'],
+                build=build_context,
+                version=env_config['version'],
+                description=env_config.get('description')
+            )
+
+    elif 'image' in env_config:
+        env = Environment(
+            name=env_config['name'],
+            version=env_config['version'],
+            image=env_config['image'],
+            description=env_config.get('description')
+        )
+
+    elif 'channels' in env_config and 'dependencies' in env_config:
+        conda_dependencies = {
+        'name': env_config['name'],
+        'channels': env_config['channels'],
+        'dependencies': env_config['dependencies']
+    }
+
+        conda_file_all = env_config['name'] + '.yml'
+        with open(conda_file_all, 'w') as file:
+            conda_file = yaml.dump(conda_dependencies, file)
+
+        # For version set to 'auto', check existing environment conda file
+        if existing_env and env_config['version'] == 'auto':
+            # Get existing conda file dependencies
+            existing_conda_data = existing_env.conda_file
+
+            # Compare dependencies
+            if conda_dependencies != existing_conda_data:
+                print(f"The conda dependencies for {env_config['name']} do not match the existing ones.")
+                # Increment version if dependencies do not match
+                existing_env.version = str(int(existing_env.version) + 1)
+                existing_env.conda_file = conda_file_all
+                env = existing_env
+            else:
+                print(f"The conda dependencies for {env_config['name']} match the existing ones.")
+        else:
+            env = Environment(
+                name=env_config['name'],
+                version=env_config['version'],
+                conda_file=conda_file_all,
+            )
+
 
 # Check if the script is invoked with necessary arguments
 if len(sys.argv) < 2:

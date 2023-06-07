c72a90f59607289eb378f92e4435f3d07e6e4046
GrannyProgramming <amcg809@gmail.com>
2023-06-07 09:57:12 +0100

    correcting "auto" version

--- .github/utils/createEnvironments.py
+++ .github/utils/createEnvironments.py
@@ -9,41 +9,47 @@ from workflowhelperfunc.workflowhelper import initialize_mlclient
 ml_client = initialize_mlclient()
 
 # 3. Define the function that creates environments according to their types specified in the JSON configuration
-# 3. Define the function that creates environments according to their types specified in the JSON configuration
 def create_environment_from_json(env_config):
-    # Check if environment already exists with the same version
+    # Initialize version
+    new_version = '1'
+
+    # Check if environment already exists
     try:
         versions = ml_client.environments.list_versions(env_config['name'])
         existing_versions = sorted([version.version for version in versions])
+        
         if existing_versions:
+            # If existing versions are found, set the new version to be the incremented last version
+            new_version = str(int(existing_versions[-1]) + 1)
             existing_env = ml_client.environments.get(name=env_config['name'], version=existing_versions[-1])
+            
             if existing_env and existing_env.conda_file == env_config.get('conda_file', '') and env_config['version'].lower() == 'auto':
                 print(f"Environment with name {env_config['name']} and version {env_config['version']} already exists and dependencies are the same.")
                 return
-            elif env_config['version'].lower() == 'auto':
-                print("Dependencies have changed or version is set to 'auto', incrementing version and updating environment.")
-                env_config['version'] = str(int(existing_versions[-1]) + 1)
-            else:
+            elif env_config['version'].lower() != 'auto':
                 print(f"Warning: SKIPPING ENVIRONMENT CREATION - Environment {env_config['name']} version and name are same but dependencies have changed. Version is not set to 'auto', so version will not be incremented.")
                 return
-        elif env_config['version'].lower() == 'auto':
-            env_config['version'] = '1'
+            else:
+                print("Dependencies have changed or version is set to 'auto', incrementing version and updating environment.")
+        elif env_config['version'].lower() != 'auto':
+            # If no existing versions and version is not auto, set new version as given version
+            new_version = env_config['version']
+
     except Exception as e:
         print("Environment does not exist or an error occurred while fetching it. Proceeding to creation/update...")
-
 
     env = None
     if 'build' in env_config:
         env = Environment(
             name=env_config['name'],
             build=BuildContext(path=env_config['build']),
-            version=env_config['version'],
+            version=new_version,
             description=env_config.get('description')
         )
     elif 'image' in env_config:
         env = Environment(
             name=env_config['name'],
-            version=env_config['version'],
+            version=new_version,
             image=env_config['image'],
             description=env_config.get('description')
         )
@@ -60,7 +66,7 @@ def create_environment_from_json(env_config):
         
         env = Environment(
             name=env_config['name'],
-            version=env_config['version'],
+            version=new_version,
             conda_file=conda_file_all,
         )
         
@@ -68,7 +74,6 @@ def create_environment_from_json(env_config):
         ml_client.environments.create_or_update(env)
     else:
         print(f"Invalid configuration for environment {env_config['name']}")
-
 
 # Check if the script is invoked with necessary arguments
 if len(sys.argv) < 2:

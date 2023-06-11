b8e85413e73719de09e8ada0d82eb45499b55c18
GrannyProgramming <amcg809@gmail.com>
2023-06-11 18:30:42 +0100

    revert + optimsised

--- .github/utils/createEnvironments.py
+++ .github/utils/createEnvironments.py
@@ -1,86 +1,63 @@
+import json
 import sys
-from azure.ai.ml.entities import Environment, BuildContext
+from azure.ai.ml.entities import Environment
 from workflowhelperfunc.workflowhelper import initialize_mlclient
 import ruamel.yaml
-import json
 
-ml_client = initialize_mlclient()
+def create_yaml_file(filename, content):
+    with open(filename, 'w') as file:
+        yaml = ruamel.yaml.YAML()
+        yaml.indent(mapping=2, sequence=4, offset=2)
+        yaml.dump(content, file)
 
-def get_environment(ml_client, env_name):
-    env_list = ml_client.environments.list()
-    for env in env_list:
-        if env.name == env_name:
-            return env
-    return None
+def prepare_env_config(config, new_version):
+    return {
+        'image': config['image'],  
+        'name': config['name'],
+        'version': new_version,
+        'conda_file': config['name'] + '.yml',
+    }
 
-def flatten_dependencies(dependencies):
-    flat_dependencies = []
-    for dep in dependencies:
-        if isinstance(dep, str):
-            flat_dependencies.append(dep)
-        elif isinstance(dep, dict):
-            for key, values in dep.items():
-                flat_dependencies.append(key)
-                flat_dependencies.extend(values)
-    return flat_dependencies
+def create_or_update_environment(ml_client, env_config):
+    conda_dependencies = {
+        'name': env_config['name'],
+        'channels': env_config['channels'],
+        'dependencies': env_config['dependencies']
+    }
 
-def environment_exists_and_same_version(ml_client, env_config):
-    existing_env = get_environment(ml_client, env_config['name'])
-    existing_env = ml_client.environments.get(name=existing_env.name, version=existing_env.latest_version)
+    create_yaml_file(env_config['name'] + '.yml', conda_dependencies)
 
-    if existing_env and existing_env.latest_version == env_config['version']:
-        print(f"Environment {existing_env.name} with version {existing_env.latest_version} already exists in AML.")
-        conda_env_dependencies = flatten_dependencies(existing_env.conda_file.get('dependencies'))
-        json_env_dependencies = flatten_dependencies(env_config.get('dependencies'))
-        if conda_env_dependencies == json_env_dependencies:
-            print("Conda dependencies in AML and JSON config are the same.")
-        else:
-            print("Conda dependencies in AML and JSON config are different.")
-        return True
-    return False
+    existing_env = next((env for env in ml_client.environments.list() if env.name == env_config['name']), None)
 
-def environment_exists_and_auto_version(ml_client, env_config):
-    existing_env = get_environment(ml_client, env_config['name'])
-    existing_env = ml_client.environments.get(name=existing_env.name, version=existing_env.latest_version)
-    if existing_env and env_config['version'] == 'auto':
-        print(f"Environment {existing_env.name} with version {existing_env.latest_version} and set to 'auto' exists in AML.")
-        conda_env_dependencies = flatten_dependencies(existing_env.conda_file.get('dependencies'))
-        json_env_dependencies = flatten_dependencies(env_config.get('dependencies'))
-        if conda_env_dependencies == json_env_dependencies:
-            print("Conda dependencies in AML and JSON config are the same.")
-        else:
-            print("Conda dependencies in AML and JSON config are different, updating environment version in AML...")
-            # update the version of the environment
-            existing_env.version = str(int(existing_env.version) + 1)
-            ml_client.environments.create_or_update(existing_env)
-        return True
-    return False
+    if existing_env:
+        existing_env = ml_client.environments.get(name=existing_env.name, version=existing_env.latest_version)
+        existing_deps = existing_env.validate().get('dependencies') if existing_env else None
 
+        if existing_deps and existing_deps == env_config['dependencies']:
+            print(f"The conda dependencies for {env_config['name']} match the existing ones.")
+            return
 
-# get the JSON file path from command line arguments
-json_file_path = sys.argv[1]
+        new_version = str(int(existing_env.version.split(':')[-1]) + 1) if env_config['version'] == 'auto' else env_config['version']
+    else:
+        new_version = '1'
 
-with open(json_file_path, 'r') as json_file:
-    json_config = json.load(json_file)
-    for env_config in json_config['conda']:
-        if 'channels' in env_config and 'dependencies' in env_config:
-            print(f"DEBUG: Processing environment {env_config['name']}...")
-            if environment_exists_and_same_version(ml_client, env_config):
-                continue
-            elif environment_exists_and_auto_version(ml_client, env_config):
-                continue
-            else:
-                print(f"Environment {env_config['name']} does not exist in AML, creating...")
-                conda_dependencies = {
-                    'name': env_config['name'],
-                    'channels': env_config['channels'],
-                    'dependencies': env_config['dependencies']
-                }
-                conda_file_all = env_config['name'] + '.yml'
-                with open(conda_file_all, 'w') as file:
-                    yaml = ruamel.yaml.YAML()
-                    yaml.indent(mapping=2, sequence=4, offset=2)
-                    yaml.dump(conda_dependencies, file)
-                # create a new environment
-                new_env = Environment(name=env_config['name'], version=env_config['version'] if env_config['version'] != 'auto' else '1', conda_file=conda_file_all)
-                ml_client.environments.create_or_update(new_env)
+    env = Environment(**prepare_env_config(env_config, new_version))
+    ml_client.environments.create_or_update(env)
+
+def main():
+    if len(sys.argv) < 2:
+        print('No configuration file provided.')
+        sys.exit(1)
+
+    print(f"DEBUG: Reading configuration file: {sys.argv[1]}")
+    print("DEBUG: Initializing ml client...")
+    ml_client = initialize_mlclient()
+
+    with open(sys.argv[1], 'r') as f:
+        config = json.load(f)
+
+    for env_config in config['conda']:
+        create_or_update_environment(ml_client, env_config)
+
+if __name__ == '__main__':
+    main()

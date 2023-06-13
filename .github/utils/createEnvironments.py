314c31969dcef523e33c920da417137525a25e8b
GrannyProgramming <amcg809@gmail.com>
2023-06-13 09:07:11 +0100

    sdsd

--- .github/utils/createEnvironments.py
+++ .github/utils/createEnvironments.py
@@ -92,13 +92,13 @@ class EnvironmentManager:
                 if existing_deps and existing_deps == env_config['dependencies']:
                     if env_config['version'] != "auto":
                         self.logger.info(f"The version for environment {env_config['name']} does not match the existing version but the conda dependencies are the same. Please update manually.")
-                    else:
-                        self.logger.info(f"The conda dependencies for {env_config['name']} match the existing ones. Environment creation/update skipped.")
+                    self.logger.info(f"The conda dependencies for {env_config['name']} match the existing ones. Environment was not updated.")
                     return
                 else:
                     new_version = env_config['version']
                     if new_version == existing_env.version:
-                        raise Exception(f"Environment {env_config['name']} with version {new_version} is already registered. Please choose a different version.")
+                        self.logger.info(f"Environment {env_config['name']} with version {new_version} is already registered. Environment was not updated.")
+                        return
                     self.logger.info(f"Updating the environment {env_config['name']}.")
             else:
                 new_version = '1'
@@ -110,6 +110,7 @@ class EnvironmentManager:
         except Exception as e:
             self.logger.error(f"Failed to create or update environment: {env_config['name']}. Error: {e}")
             raise
+
 
 def main() -> None:
     """Entry point for the script."""

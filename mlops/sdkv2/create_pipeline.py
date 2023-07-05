3d2bb83956703ca01e3969294fdc1ac5dd71036c
GrannyProgramming <amcg809@gmail.com>
2023-07-05 16:54:09 +0100

    Update create_pipeline.py to include components loading and initialization

--- mlops/sdkv2/create_pipeline.py
+++ mlops/sdkv2/create_pipeline.py
@@ -3,7 +3,7 @@ import json
 from azure.ai.ml.dsl import pipeline
 from azure.ai.ml import load_component, Input
 from workflowhelperfunc.workflowhelper import initialize_mlclient
- 
+
 def load_component_by_name(name):
     existing_component = next((component for component in ml_client.components.list() 
                             if component.name == name), None)
@@ -18,34 +18,41 @@ def load_component_by_name(name):
     
     return component_func
 
+ml_client = initialize_mlclient()
+
+json_file_path = sys.argv[1]
+
+with open(json_file_path, 'r') as f:
+    config = json.load(f)
+
+# load components mentioned in the JSON file
+components = {}
+for pipeline_config in config['pipelines']:
+    for component_name in pipeline_config['pipeline_components']:
+        component_func = load_component_by_name(component_name)
+        if component_func is None:
+            print(f"Failed to load component: {component_name}")
+        else:
+            components[component_name] = component_func
+
 @pipeline
 def data_pipeline(raw_data: Input):
     """pipeline component with data prep and transformation defined via yaml."""
-    prep_node = prep_taxi_data(raw_data=raw_data)
-    transform_node = taxi_feature_engineering(clean_data=prep_node.outputs.prep_data)
+    prep_node = components['prep_taxi_data'](raw_data=raw_data)
+    transform_node = components['taxi_feature_engineering'](clean_data=prep_node.outputs.prep_data)
     return {"train_data": transform_node.outputs.transformed_data}
-
 
 @pipeline
 def train_pipeline(train_data: Input, compute_train_node: str):
-    train_linear_regression_model = load_component_by_name('train_linear_regression_model')
-    if train_linear_regression_model is None:
-        return None
-    train_node = train_linear_regression_model(train_data=train_data, test_split_ratio=0.2)
+    train_node = components['train_linear_regression_model'](train_data=train_data, test_split_ratio=0.2)
     train_node.compute = compute_train_node
 
-    predict_taxi_fares = load_component_by_name('predict_taxi_fares')
-    if predict_taxi_fares is None:
-        return None
-    predict_node = predict_taxi_fares(
+    predict_node = components['predict_taxi_fares'](
         model_input=train_node.outputs.model_output,
         test_data=train_node.outputs.test_data,
     )
 
-    score_model = load_component_by_name('score_model')
-    if score_model is None:
-        return None
-    score_node = score_model(
+    score_node = components['score_model'](
         predictions=predict_node.outputs.predictions,
         model=train_node.outputs.model_output,
     )
@@ -54,7 +61,6 @@ def train_pipeline(train_data: Input, compute_train_node: str):
         "predictions": predict_node.outputs.predictions,
         "score_report": score_node.outputs.score_report,
     }
-
 
 @pipeline
 def nyc_taxi_data_regression(pipeline_raw_data: Input, compute_train_node: str):
@@ -73,16 +79,7 @@ def nyc_taxi_data_regression(pipeline_raw_data: Input, compute_train_node: str):
         "pipeline_job_score_report": train_pipeline_node.outputs.score_report,
     }
 
-ml_client = initialize_mlclient()
-
-json_file_path = sys.argv[1]
-
-with open(json_file_path, 'r') as f:
-    config = json.load(f)
-
 for pipeline_config in config['pipelines']:
-    components = [load_component_by_name(name) for name in pipeline_config['pipeline_components']]
-
     pipeline_job = nyc_taxi_data_regression(
         pipeline_raw_data=Input(type="uri_folder", path=pipeline_config['file_paths']['raw_data_path']),
         compute_train_node=pipeline_config['compute'],

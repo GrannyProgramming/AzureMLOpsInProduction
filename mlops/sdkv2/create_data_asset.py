5038de154813e2c6eeb7b0f0567661b51fcf3159
grannyprogramming <amcg809@gmail.com>
2023-07-09 20:41:37 +0100

    Commit message: Update create_data_asset.py to check required keys and support data types

--- mlops/sdkv2/create_data_asset.py
+++ mlops/sdkv2/create_data_asset.py
@@ -4,114 +4,72 @@ from azure.ai.ml.entities import Data
 from azure.ai.ml.constants import AssetTypes
 from workflowhelperfunc.workflowhelper import setup_logger, log_event, initialize_mlclient, load_config
 
-
-
-class DataAssetManager:
-    """Manage Azure ML Data Assets."""
-
-    def __init__(self, config_file: str) -> None:
-        """
-        Initialize the DataAssetManager.
-
-        Args:
-            config_file (str): Path to the configuration file.
-        """
-        self.config_file: str = config_file
-        self.client = initialize_mlclient()
-        self.existing_assets: dict = {}
-        self.logger = setup_logger(__name__)
-
-    def check_asset_exists(self, data_name: str) -> bool:
-        """
-        Check if the data asset already exists.
-
-        Args:
-            data_name (str): Name of the data asset.
-
-        Returns:
-            bool: True if the data asset exists, False otherwise.
-        """
-        if data_name in self.existing_assets:
-            return True
-
-        existing_asset = self.client.data.get(data_name)
-        if existing_asset is not None:
-            self.existing_assets[data_name] = existing_asset
-            return True
-
+# Check if the required keys are in the configuration
+def check_required_keys(data_config, logger):
+    required_keys = ['type', 'name']
+    missing_keys = [key for key in required_keys if key not in data_config]
+    if missing_keys:
+        log_event(logger, 'error', f"Data config is missing required keys: {', '.join(missing_keys)}.")
         return False
+    return True
 
-    def create_data_asset(self, data_config: dict) -> None:
-        """
-        Create or update a data asset.
+# Check if the type of the data is supported
+def check_data_type(data_config, logger):
+    data_types = {
+        "uri_file": AssetTypes.URI_FILE,
+        "uri_folder": AssetTypes.URI_FOLDER,
+        "mltable": AssetTypes.MLTABLE  
+    }
+    data_type = data_config["type"].lower()
+    if data_type not in data_types:
+        log_event(logger, 'error', f"{data_type.capitalize()} data type is not supported.")
+        return None
+    return data_types[data_type]
 
-        Args:
-            data_config (dict): Configuration data for the data asset.
-        """
-        required_keys = ['type', 'name']
-        missing_keys = [key for key in required_keys if key not in data_config]
+# Create or update the data asset
+def create_or_update_asset(client, data_config, data_type, logger):
+    data_name = data_config["name"]
+    data_version = data_config.get('version', datetime.now().strftime('%Y%m%d'))
+    try:
+        data_entity = Data(
+            name=data_name,
+            version=data_version,
+            type=data_type,
+            description=data_config.get("description", ""),
+            path=data_config.get("path", ""),
+        )
+        client.data.create_or_update(data_entity)
+        log_event(logger, 'info', f"{data_config['type'].capitalize()} data asset '{data_name}' created or updated with version {data_entity.version}.")
+        return data_entity
+    except Exception as e:
+        log_event(logger, 'error', f"Failed to create or update {data_config['type'].capitalize()} data asset '{data_name}': {str(e)}")
+        return None
 
-        if missing_keys:
-            log_event(self.logger, 'error', f"Data config is missing required keys: {', '.join(missing_keys)}.")
-            return
+def create_data_asset(client, existing_assets, data_config, logger):
+    if not check_required_keys(data_config, logger):
+        return
+    data_type = check_data_type(data_config, logger)
+    if data_type is None:
+        return
+    data_entity = create_or_update_asset(client, data_config, data_type, logger)
+    if data_entity is not None:
+        existing_assets[data_config['name']] = data_entity
 
-        data_types = {
-            "uri_file": AssetTypes.URI_FILE,
-            "uri_folder": AssetTypes.URI_FOLDER,
-            "mltable": AssetTypes.MLTABLE  
-        }
+def execute(client, config_file, logger):
+    config = load_config(config_file)
+    existing_assets = {}
 
-        data_type = data_config["type"].lower()
-        data_name = data_config["name"]
+    for data_config in config["data"]:
+        create_data_asset(client, existing_assets, data_config, logger)
 
-        if data_type not in data_types:
-            log_event(self.logger, 'error', f"{data_type.capitalize()} data type is not supported.")
-            return
-
-        # Get version from data_config or generate a new one
-        data_version = data_config.get('version', datetime.now().strftime('%Y%m%d'))
-
-        try:
-            data_entity = Data(
-                name=data_name,
-                version=data_version,
-                type=data_types[data_type],
-                description=data_config.get("description", ""),
-                path=data_config.get("path", ""),
-            )
-            self.client.data.create_or_update(data_entity)
-            log_event(self.logger, 'info', f"{data_type.capitalize()} data asset '{data_name}' created or updated with version {data_entity.version}.")
-            self.existing_assets[data_name] = data_entity
-        except Exception as e:
-            log_event(self.logger, 'error', f"Failed to create or update {data_type.capitalize()} data asset '{data_name}': {str(e)}")
-
-    def execute(self) -> None:
-        """
-        Main method to run the program.
-
-        Load the configuration and create data assets based on the configuration.
-        """
-        config = load_config(self.config_file)
-
-        for data_config in config["data"]:
-            self.create_data_asset(data_config)
-
-
-def main(config_file: str) -> None:
-    """
-    Main execution of the script: Initialize the DataAssetManager and execute it.
-
-    Args:
-        config_file (str): Configuration file path.
-    """
+def main(config_file):
     logger = setup_logger(__name__)
 
     try:
-        manager = DataAssetManager(config_file)
-        manager.execute()
+        client = initialize_mlclient()
+        execute(client, config_file, logger)
     except Exception as e:
         log_event(logger, 'error', f"An error occurred: {str(e)}")
-
 
 if __name__ == "__main__":
     main(sys.argv[1])

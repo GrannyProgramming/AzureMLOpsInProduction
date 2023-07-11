b4ff975ac4b5d438c2511ec238d25a57927f86c3
grannyprogramming <amcg809@gmail.com>
2023-07-11 12:19:49 +0100

    Update create_component.py to use environment string instead of reference

--- mlops/sdkv2/create_component.py
+++ mlops/sdkv2/create_component.py
@@ -15,28 +15,6 @@ def generate_references(data):
             references[key] = value
     return references
 
-def replace_references(data, original):
-    if isinstance(data, dict):
-        if 'reference' in data and isinstance(data['reference'], str):
-            parts = data['reference'].split('.')
-            ref_data = original
-            for part in parts:
-                if isinstance(ref_data, dict) and part in ref_data:
-                    ref_data = ref_data.get(part)
-                else:
-                    ref_data = data  # Return the original dictionary if the reference cannot be resolved
-                    break
-            return ref_data
-        else:
-            new_dict = {}
-            for key, value in data.items():
-                new_dict[key] = replace_references(value, original)
-            return new_dict
-    elif isinstance(data, list):
-        return [replace_references(item, original) for item in data]
-    else:
-        return data
-
 def create_component_from_json(component, references):
     print("Creating component", component["name"])
     print("Component inputs:", component["inputs"])
@@ -64,7 +42,7 @@ def create_component_from_json(component, references):
         outputs=outputs,
         code=code_filepath,
         command=command_str,  # actual command should be the value
-        environment=component['env'],  # reference replaced with actual environment
+        environment=component['env']['env'],  # reference replaced with actual environment string
     )
 
     return new_component

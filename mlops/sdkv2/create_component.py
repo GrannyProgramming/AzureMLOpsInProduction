6a92aff95eaf2d82d827363f3b66534bcbeba587
grannyprogramming <amcg809@gmail.com>
2023-07-12 19:18:31 +0100

    Update create_component.py to comment out print statements

--- mlops/sdkv2/create_component.py
+++ mlops/sdkv2/create_component.py
@@ -52,7 +52,7 @@ def create_component_from_json(component, references):
         command=command_str,
         environment=environment,
     )
-    print("new_component variable: ", new_component)
+    # print("new_component variable: ", new_component)
     return new_component
 
 
@@ -60,9 +60,9 @@ def create_components_from_json_file(json_file):
     with open(json_file, 'r') as f:
         data = json.load(f)
     references = generate_references(data)
-    print("references variable: ",references)
+    # print("references variable: ",references)
     resolved_json = replace_references(copy.deepcopy(data), references)
-    print("resolved_json variable: ", resolved_json)
+    # print("resolved_json variable: ", resolved_json)
     components = [create_component_from_json(component, references) for component in resolved_json['components_framework'].values()]
     return components
 
